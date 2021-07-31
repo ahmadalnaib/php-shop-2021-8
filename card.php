779551b647a02e37d1a0b67ec89ae7ebee156b6a
ahmedhabeeb73 <alnaib888@gmail.com>
2021-07-31 18:56:40 +0200

    connect with db and fetch the data

---
+++ card.php
@@ -0,0 +1,11 @@
+<div class="card">
+<img src="https://via.placeholder.com/50x50" alt="" class="card-img-top">
+   <div class="card-body">
+   test
+   </div>
+
+   <div class="card-footer">
+   <a href="" class="btn btn-primary">details</a>
+   <a href="" class="btn btn-success">In den warenkorb</a>
+   </div>
+   </div>
--- index.php
+++ index.php
@@ -1,3 +1,39 @@
-<?php 
+<?php
 
-echo "ahmed";
+$username="root";
+$password="";
+$dsn="mysql:host=localhost;dbname=shop1;charset=utf8";
+$db=new PDO($dsn,$username,$password);
+
+$sql="SELECT * FROM products";
+$result= $db->query($sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>shop</title>
+</head>
+<body>
+<div class="container" id="products">
+ <div class="row">
+ <?php while($row = $result->fetch()): ?>
+ <div class="col">
+ <?php include 'card.php' ?>
+ </div>
+ <?php endwhile; ?>
+ </div>
+</div> 
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
